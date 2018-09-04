0d93b732cae88b36c64f37689c6d8932580f7050
Martin Bryant <mail@martinbryant.io>
2018-09-04 15:17:35 +0100

    apiMiddleware can now handle multiple requests to 1 or more urls

--- src/middleware/api-middleware.js
+++ src/middleware/api-middleware.js
@@ -6,9 +6,28 @@ export const apiMiddleware = ({ dispatch }) => next => action => {
         const { url, method, feature } = action.meta
         const successResponse = res => dispatch(apiSuccess(res, feature))
         const errorResponse = err => dispatch(apiError(err, feature))
-        return fetch(url, { method })
-            .then(isResponseOk)
-            .then(convertToJson)
+        let urls = []
+        if (typeof url === 'string') {
+            urls = [...urls,
+                url]
+        } else {
+            urls = url
+        }
+        const getAllUrls = async (urls) => {
+            try {
+                var data = await Promise.all(urls.map(url =>
+                    fetch(url, { method })
+                        .then(isResponseOk)
+                        .then(convertToJson)
+                        .catch(errorResponse)))
+                return data
+            }
+            catch (error) {
+                return error
+            }
+        }
+        return getAllUrls(urls)
+            .then(flattenResponse)
             .then(successResponse)
             .catch(errorResponse)
     }
@@ -16,5 +35,8 @@ export const apiMiddleware = ({ dispatch }) => next => action => {
 
 export default apiMiddleware
 
-const convertToJson = res => res.json()
 const isResponseOk = res => res.ok ? Promise.resolve(res) : Promise.reject(Error('Network Response Error'))
+
+const convertToJson = res => res.json()
+
+const flattenResponse = res => Promise.resolve(res.reduce((acc = [], curr) => [...acc, ...curr]))
