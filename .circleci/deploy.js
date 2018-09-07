5c61110c7c468c8c2189fbeb51952f2a190fbd21
Martin Bryant <mail@martinbryant.io>
2018-09-07 21:01:18 +0100

    Auto deploy to ftp using circleci

---
+++ .circleci/deploy.js
@@ -0,0 +1,16 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+    user: process.env.FTPUSER,
+    password: process.env.FTPPASS,
+    host: process.env.FTPHOST,
+    port: 21,
+    localRoot: __dirname + "/build",
+    remoteRoot: "/public_html/beer-engineer",
+    include: ['*']
+}
+
+ftpDeploy.deploy(config)
+    .then(res => console.log('finished'))
+    .catch(err => console.log(err))
