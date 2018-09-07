7b54315a8444d650a7e959806dd13592e11cbc0c
Martin Bryant <mail@martinbryant.io>
2018-09-07 22:06:59 +0100

    Amend deploy file

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -6,7 +6,7 @@ var config = {
     password: process.env.FTPPASS,
     host: process.env.FTPHOST,
     port: 21,
-    localRoot: __dirname + "/../build/",
+    localRoot: __dirname + "/build",
     remoteRoot: "/public_html/beer-engineer/",
     include: ['*']
 }
