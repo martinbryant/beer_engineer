3829b35a11af4b4703c5b5a0c97b3a3a5a94bdb2
Martin Bryant <mail@martinbryant.io>
2018-09-13 20:16:14 +0100

    use radio controls to control the value of the get random button

--- src/components/random-bar.js
+++ src/components/random-bar.js
@@ -3,39 +3,47 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 
-import { fetchBeerRandom } from '../actions/beer-actions'
+import { fetchBeerRandom } from '../actions/beer-actions';
+import { updateNoOfRandomBeers } from '../actions/ui-actions'
 
 const styles = {
     formControl: {
         minWidth: 135
     },
     button: {
-        marginRight: 10,
-        marginLeft: 10,
-        minWidth: 90
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        marginRight: 20,
+        minWidth: 90,
+        maxHeight: 20
     }
 }
 
-const RandomBar = ({ classes, getRandomBeers }) => (
+const RandomBar = ({ classes, noOfRandomBeers, getRandomBeers, updateNoOfRandomBeers }) => (
     <Grid container justify="center">
         <Button
             className={classes.button}
-            onClick={e => getRandomBeers(5)}
+            onClick={e => getRandomBeers(noOfRandomBeers)}
             variant="contained"
             size="small"
             color="primary">
-            5 Random
+            {noOfRandomBeers} Random
         </Button>
-        <Button
-            className={classes.button}
-            onClick={e => getRandomBeers(10)}
-            variant="contained"
-            size="small"
-            color="primary">
-            10 Random
-        </Button>
+        <RadioGroup
+            value={noOfRandomBeers}
+            onChange={(e, value) => updateNoOfRandomBeers(parseInt(value))}>
+            <FormControlLabel value={5} control={<Radio />} label={5} />
+            <FormControlLabel value={10} control={<Radio />} label={10} />
+        </RadioGroup>
 
     </Grid>
 )
@@ -55,6 +63,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
     getRandomBeers(noOfBeers) {
         dispatch(fetchBeerRandom(noOfBeers))
+    },
+    updateNoOfRandomBeers(noOfBeers) {
+        dispatch(updateNoOfRandomBeers(noOfBeers))
     }
 })
 
