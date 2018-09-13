b849d36333ec86e3c138f8fcca7da26cf4e8b479
Martin Bryant <mail@martinbryant.io>
2018-09-13 19:45:10 +0100

    use buttons to get random beers, either 5 or 10

--- src/components/random-bar.js
+++ src/components/random-bar.js
@@ -3,50 +3,40 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Grid from '@material-ui/core/Grid';
 
-import { fetchBeerRandom } from '../actions/beer-actions';
-import { updateNoOfRandomBeers } from '../actions/ui-actions'
+import { fetchBeerRandom } from '../actions/beer-actions'
 
 const styles = {
     formControl: {
         minWidth: 135
     },
     button: {
+        marginRight: 10,
+        marginLeft: 10,
         minWidth: 90
     }
 }
 
-const RandomBar = ({ classes, noOfRandomBeers, getRandomBeers, updateNoOfRandomBeers }) => (
+const RandomBar = ({ classes, getRandomBeers }) => (
     <Grid container justify="center">
         <Button
             className={classes.button}
-            onClick={e => getRandomBeers(noOfRandomBeers)}
+            onClick={e => getRandomBeers(5)}
             variant="contained"
             size="small"
             color="primary">
-            {noOfRandomBeers} Random
+            5 Random
         </Button>
-        <FormControl className={classes.formControl}>
-            <InputLabel >How many beers?</InputLabel>
-            <Select
-                value={noOfRandomBeers}
-                onChange={e => updateNoOfRandomBeers(e.target.value)}
-                inputProps={{
-                    name: 'number',
-                    id: 'beers',
-                }}
-            >
-                <MenuItem value={5}>5</MenuItem>
-                <MenuItem value={10}>10</MenuItem>
-                <MenuItem value={15}>15</MenuItem>
-                <MenuItem value={20}>20</MenuItem>
-            </Select>
-        </FormControl>
+        <Button
+            className={classes.button}
+            onClick={e => getRandomBeers(10)}
+            variant="contained"
+            size="small"
+            color="primary">
+            10 Random
+        </Button>
+
     </Grid>
 )
 
@@ -65,9 +55,6 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
     getRandomBeers(noOfBeers) {
         dispatch(fetchBeerRandom(noOfBeers))
-    },
-    updateNoOfRandomBeers(noOfBeers) {
-        dispatch(updateNoOfRandomBeers(noOfBeers))
     }
 })
 
